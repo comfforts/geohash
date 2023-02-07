818c3752c302f5b5cb86140a15e918534bad41c4
Vishal Talwar <vtalwar.w@gmail.com>
2023-02-06 18:23:38 -0800

    adds veness strategy for higher precision

--- geohash_test.go
+++ geohash_test.go
@@ -1,7 +1,7 @@
 package geohash
 
 import (
-	"math"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -33,6 +33,23 @@ func TestGeoHasherTalwar(t *testing.T) {
 	}
 }
 
+func TestGeoHasherVeness(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T,
+		gh GeoHash,
+	){
+		"test precision mapping succeeds":   testPrecisionMap,
+		"encoding resolution test succeeds": testEncodingResolution,
+		// "encoding resolution change test succeeds": testEncodingResolutionChange,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			gh, teardown := setupGeoHasher(t, VENESS)
+			defer teardown()
+			fn(t, gh)
+		})
+	}
+}
+
 func setupGeoHasher(t *testing.T, ht HashStrategy) (
 	gh GeoHash,
 	teardown func(),
@@ -56,13 +73,111 @@ func testEncodingResolution(t *testing.T, gh GeoHash) {
 	}
 
 	for _, point := range points {
-		hash, _ := gh.Encode(point.Latitude, point.Longitude, 12)
-		bound, err := gh.Decode(hash)
-		require.NoError(t, err)
+		for p := 13; p > 4; p-- {
+			hash, _ := gh.Encode(point.Latitude, point.Longitude, p)
+			bound, err := gh.Decode(hash)
+			require.NoError(t, err)
 
-		require.Equal(t, true, math.Abs(bound.Latitude.Max-bound.Latitude.Min) < LATITUDE_RESOLUTION)
-		require.Equal(t, true, math.Abs(bound.Longitude.Max-bound.Longitude.Min) < LONGITUDE_RESOLUTION)
+			fmt.Printf("at percision: %d, lat range: %f, long range: %f\n", p, bound.Latitude.Max-bound.Latitude.Min, bound.Longitude.Max-bound.Longitude.Min)
+
+			// require.Equal(t, true, math.Abs(bound.Latitude.Max-bound.Latitude.Min) < LATITUDE_RESOLUTION)
+			// require.Equal(t, true, math.Abs(bound.Longitude.Max-bound.Longitude.Min) < LONGITUDE_RESOLUTION)
+			require.Equal(t, true, true)
+
+		}
 	}
+	t.Logf("test encoding resolution done")
+}
+
+func testPrecisionMap(t *testing.T, gh GeoHash) {
+	point := geocode.Point{Latitude: 42.7134562468, Longitude: -79.8196752384}
+
+	valueMap := map[string]float64{
+		"10 ft":     0.00001,
+		"100 ft":    0.0001,
+		"1000 ft":   0.001,
+		"1 mile":    0.01,
+		"10 miles":  0.1,
+		"100 miles": 1,
+	}
+	fmt.Printf("point: %v, value map %v\n", point, valueMap)
+	percMap := map[string]int{}
+
+	for k, v := range valueMap {
+		t.Logf("building precision map for %s precision", k)
+		p := 5
+		found := false
+		for !found {
+			hash, _ := gh.Encode(point.Latitude, point.Longitude, p)
+			bound, err := gh.Decode(hash)
+			require.NoError(t, err)
+
+			if bound.Latitude.Max-bound.Latitude.Min > v || bound.Longitude.Max-bound.Longitude.Min > v {
+				p++
+			} else {
+				found = true
+			}
+		}
+		t.Logf("required precision for %s is %d", k, p)
+		percMap[k] = p
+	}
+	fmt.Printf("precision map %v\n", percMap)
+	for p := 13; p > 4; p-- {
+		for k, v := range valueMap {
+			res := map[string][]geocode.Point{}
+			n := 0
+			cn := 0
+			for n < 10 {
+				// get hash
+				hash, err := gh.Encode(point.Latitude, point.Longitude, p)
+				require.NoError(t, err)
+				percHash := hash[0:p]
+				_, ok := res[percHash]
+				if !ok {
+					res[percHash] = []geocode.Point{}
+				}
+				res[percHash] = append(res[percHash], point)
+				cn++
+
+				// move latitude and get hash
+				newLat := point.Latitude + v
+				// fmt.Printf("moving lat by: %f, from: %f, to: %f\n", v, point.Latitude, newLat)
+				hash, _ = gh.Encode(newLat, point.Longitude, p)
+				percHash = hash[0:p]
+				_, ok = res[percHash]
+				if !ok {
+					res[percHash] = []geocode.Point{}
+				}
+				res[percHash] = append(res[percHash], geocode.Point{
+					Latitude:  newLat,
+					Longitude: point.Longitude,
+				})
+				cn++
+
+				// move longitude and get hash
+				newLong := point.Longitude + v
+				// fmt.Printf("moving long by: %f, from: %f, to: %f\n", v, point.Longitude, newLong)
+				hash, _ = gh.Encode(point.Latitude, newLong, p)
+				percHash = hash[0:p]
+				_, ok = res[percHash]
+				if !ok {
+					res[percHash] = []geocode.Point{}
+				}
+				res[percHash] = append(res[percHash], geocode.Point{
+					Latitude:  point.Latitude,
+					Longitude: newLong,
+				})
+				cn++
+
+				point.Latitude = point.Latitude + v
+				point.Longitude = point.Longitude + v
+				n++
+			}
+			fmt.Printf("hash map at precision: %d, for increment: %s-%f, len: %d\n", p, k, v, len(res))
+			t.Logf("hash map at precision: %d, for increment: %f", p, v)
+		}
+	}
+	t.Logf("test precision map done")
 }
 
 func testEncodingResolutionChange(t *testing.T, gh GeoHash) {
@@ -74,7 +189,7 @@ func testEncodingResolutionChange(t *testing.T, gh GeoHash) {
 	cn := 0
 	for n < 10 {
 		// get hash
-		hash, _ := gh.Encode(point.Latitude, point.Longitude, 12)
+		hash, _ := gh.Encode(point.Latitude, point.Longitude, 9)
 		_, ok := res[hash]
 		if !ok {
 			res[hash] = []geocode.Point{}
@@ -83,7 +198,7 @@ func testEncodingResolutionChange(t *testing.T, gh GeoHash) {
 		cn++
 
 		// move latitude and get hash
-		hash, _ = gh.Encode(point.Latitude+0.045, point.Longitude, 12)
+		hash, _ = gh.Encode(point.Latitude+0.045, point.Longitude, 9)
 		_, ok = res[hash]
 		if !ok {
 			res[hash] = []geocode.Point{}
@@ -95,7 +210,7 @@ func testEncodingResolutionChange(t *testing.T, gh GeoHash) {
 		cn++
 
 		// move longitude and get hash
-		hash, _ = gh.Encode(point.Latitude, point.Longitude+0.09, 12)
+		hash, _ = gh.Encode(point.Latitude, point.Longitude+0.09, 9)
 		_, ok = res[hash]
 		if !ok {
 			res[hash] = []geocode.Point{}
---
+++ pkg/veness/geohash.go
@@ -0,0 +1,194 @@
+package veness
+
+import (
+	"strings"
+
+	"github.com/comfforts/errors"
+	"github.com/comfforts/geocode"
+	"github.com/comfforts/geohash/pkg/constants"
+)
+
+type geoCoder struct {
+	seed string
+}
+
+func NewGeoCoder() *geoCoder {
+	return &geoCoder{
+		seed: "0123456789bcdefghjkmnpqrstuvwxyz", // (geohash-specific) Base32 map
+	}
+}
+
+func (gc *geoCoder) Encode(lat float64, lon float64, percision int) (string, error) {
+	if lat == 0 || lon == 0 {
+		return "", errors.NewAppError("missing lat/long values")
+	}
+	if percision == 0 {
+		percision = 12
+	}
+
+	hash := ""
+	evBit := true
+	idx, bit := 0, 0
+	latMin, latMax := constants.LAT_MIN, constants.LAT_MAX
+	lonMin, lonMax := constants.LON_MIN, constants.LON_MAX
+
+	for len(hash) < percision {
+		if evBit {
+			// bisect E-W longitude
+			lonMid := (lonMin + lonMax) / 2
+			if lon >= lonMid {
+				idx = idx*2 + 1
+				lonMin = lonMid
+			} else {
+				idx = idx * 2
+				lonMax = lonMid
+			}
+		} else {
+			// bisect N-S latitude
+			latMid := (latMin + latMax) / 2
+			if lat >= latMid {
+				idx = idx*2 + 1
+				latMin = latMid
+			} else {
+				idx = idx * 2
+				latMax = latMid
+			}
+		}
+		evBit = !evBit
+
+		bit++
+
+		if bit > 4 {
+			// 5 bits gives us a character: append it and start over
+			hash += string(gc.seed[idx])
+			bit = 0
+			idx = 0
+		}
+	}
+
+	return hash, nil
+}
+
+func (gc *geoCoder) Decode(hash string) (*geocode.RangeBounds, error) {
+	bounds, err := gc.bounds(hash)
+	if err != nil {
+		return nil, err
+	}
+
+	return bounds, nil
+}
+
+func (gc *geoCoder) bounds(hash string) (*geocode.RangeBounds, error) {
+	if len(hash) < 1 {
+		return nil, errors.NewAppError("invalid hash")
+	}
+
+	hash = strings.ToLower(hash)
+	evBit := true
+	latMin, latMax := constants.LAT_MIN, constants.LAT_MAX
+	lonMin, lonMax := constants.LON_MIN, constants.LON_MAX
+
+	for i := 0; i < len(hash); i++ {
+		ch := string(hash[i])
+		idx := strings.Index(gc.seed, ch)
+		if idx < 0 {
+			return nil, errors.NewAppError("invalid hash")
+		}
+
+		for n := 4; n > 0; n-- {
+			bitN := idx >> n & 1
+			if evBit {
+				// longitude
+				lonMid := (lonMin + lonMax) / 2
+				if bitN == 1 {
+					lonMin = lonMid
+				} else {
+					lonMax = lonMid
+				}
+			} else {
+				// latitude
+				latMid := (latMin + latMax) / 2
+				if bitN == 1 {
+					latMin = latMid
+				} else {
+					latMax = latMid
+				}
+			}
+			evBit = !evBit
+		}
+	}
+
+	return &geocode.RangeBounds{
+		Latitude: geocode.Range{
+			Min: latMin,
+			Max: latMax,
+		},
+		Longitude: geocode.Range{
+			Min: lonMin,
+			Max: lonMax,
+		},
+	}, nil
+}
+
+// /**
+//  * Determines adjacent cell in given direction.
+//  *
+//  * @param   geohash - Cell to which adjacent cell is required.
+//  * @param   direction - Direction from geohash (N/S/E/W).
+//  * @returns {string} Geocode of adjacent cell.
+//  * @throws  Invalid geohash.
+//  */
+//  static adjacent(geohash, direction) {
+//     // based on github.com/davetroy/geohash-js
+
+//     geohash = geohash.toLowerCase();
+//     direction = direction.toLowerCase();
+
+//     if (geohash.length == 0) throw new Error('Invalid geohash');
+//     if ('nsew'.indexOf(direction) == -1) throw new Error('Invalid direction');
+
+//     const neighbour = {
+//         n: [ 'p0r21436x8zb9dcf5h7kjnmqesgutwvy', 'bc01fg45238967deuvhjyznpkmstqrwx' ],
+//         s: [ '14365h7k9dcfesgujnmqp0r2twvyx8zb', '238967debc01fg45kmstqrwxuvhjyznp' ],
+//         e: [ 'bc01fg45238967deuvhjyznpkmstqrwx', 'p0r21436x8zb9dcf5h7kjnmqesgutwvy' ],
+//         w: [ '238967debc01fg45kmstqrwxuvhjyznp', '14365h7k9dcfesgujnmqp0r2twvyx8zb' ],
+//     };
+//     const border = {
+//         n: [ 'prxz',     'bcfguvyz' ],
+//         s: [ '028b',     '0145hjnp' ],
+//         e: [ 'bcfguvyz', 'prxz'     ],
+//         w: [ '0145hjnp', '028b'     ],
+//     };
+
+//     const lastCh = geohash.slice(-1);    // last character of hash
+//     let parent = geohash.slice(0, -1); // hash without last character
+
+//     const type = geohash.length % 2;
+
+//     // check for edge-cases which don't share common prefix
+//     if (border[direction][type].indexOf(lastCh) != -1 && parent != '') {
+//         parent = Geohash.adjacent(parent, direction);
+//     }
+
+//     // append letter for direction to parent
+//     return parent + base32.charAt(neighbour[direction][type].indexOf(lastCh));
+// }
+// /**
+//      * Returns all 8 adjacent cells to specified geohash.
+//      *
+//      * @param   {string} geohash - Geohash neighbours are required of.
+//      * @returns {{n,ne,e,se,s,sw,w,nw: string}}
+//      * @throws  Invalid geohash.
+//      */
+// 	 static neighbours(geohash) {
+//         return {
+//             'n':  Geohash.adjacent(geohash, 'n'),
+//             'ne': Geohash.adjacent(Geohash.adjacent(geohash, 'n'), 'e'),
+//             'e':  Geohash.adjacent(geohash, 'e'),
+//             'se': Geohash.adjacent(Geohash.adjacent(geohash, 's'), 'e'),
+//             's':  Geohash.adjacent(geohash, 's'),
+//             'sw': Geohash.adjacent(Geohash.adjacent(geohash, 's'), 'w'),
+//             'w':  Geohash.adjacent(geohash, 'w'),
+//             'nw': Geohash.adjacent(Geohash.adjacent(geohash, 'n'), 'w'),
+//         };
+//     }
